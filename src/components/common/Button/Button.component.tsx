5da7900973dd3a8fe122cca7f18efa832b891f8f
YONGJAE LEE <dev.yongjaelee@cakecorp.com>
2022-06-10 16:36:27 +0900

    공통: 버튼 컴포넌트 추가
    * react/jsx-no-useless-fragment rule 추가

---
+++ src/components/common/Button/Button.component.tsx
@@ -0,0 +1,53 @@
+import React, {
+  ButtonHTMLAttributes,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from 'react';
+import cn from 'classnames';
+import styles from './Button.module.scss';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  labelText?: string;
+}
+
+export interface ParentRef {
+  focus: () => void;
+}
+
+const Button = (
+  {
+    children,
+    className = '',
+    disabled = false,
+    fullWidth = false,
+    labelText = '',
+    ...restProps
+  }: ButtonProps,
+  parentRef: React.Ref<ParentRef>,
+) => {
+  const childRef = useRef<HTMLButtonElement>(null);
+
+  useImperativeHandle(parentRef, () => ({
+    focus: () => {
+      childRef.current?.focus();
+    },
+  }));
+
+  return (
+    <button
+      type="button"
+      ref={childRef}
+      className={cn(styles.button, { [styles['full-width']]: fullWidth })}
+      disabled={disabled}
+      {...restProps}
+    >
+      <span>{labelText ? <>{labelText}</> : children}</span>
+    </button>
+  );
+};
+
+export default forwardRef<ParentRef, ButtonProps>(Button);
---
+++ src/components/common/Button/Button.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Button, { ButtonProps } from './Button.component';
+
+export default {
+  title: 'common/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args: ButtonProps) => (
+  <Button {...args} />
+);
+export const ButtonBasic = Template.bind({});
+ButtonBasic.args = {
+  labelText: 'TEST',
+};
+
+export const ButtonDisabled = Template.bind({});
+ButtonDisabled.args = {
+  disabled: true,
+  labelText: 'TEST',
+};
+
+export const ButtonFullWidth = Template.bind({});
+ButtonFullWidth.args = {
+  fullWidth: true,
+  labelText: 'TEST',
+};
