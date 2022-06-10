2ffa3f6ce723c3292682b8ae4a836e2792f4a86a
YONGJAE LEE <dev.yongjaelee@cakecorp.com>
2022-06-10 16:39:04 +0900

    공통: normalize.css 적용 및 베이스 페이지 추가

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { Button } from '~/components';
+
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Test</title>
+      <meta name="description" content="Test" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <Button>Main</Button>
+    </main>
+
+    <footer>Footer</footer>
+  </div>
+);
+
+export default Home;
