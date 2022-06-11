db749e5e68922e852f8479ad5ced45771aaf6ccc
YONGJAE LEE <dev.yongjaelee@cakecorp.com>
2022-06-11 14:39:24 +0900

    공통: storybook 관련 설정 변경
    https://storybook.js.org/blog/get-started-with-storybook-and-next-js/
    * .gitignore에 storybook-static 추가
    * alias에 public 제거

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,7 +16,6 @@ module.exports = {
   webpackFinal: async (config) => {
     config.resolve.alias = {
       "~": path.resolve(__dirname, "../src"),
-      public: path.resolve(__dirname, "../public"),
     };
 
     return config;
