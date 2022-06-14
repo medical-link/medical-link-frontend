97acd7145db35ca8cdb61b6659552f56e015df9c
YONGJAE LEE <dev.yongjaelee@cakecorp.com>
2022-06-14 21:44:22 +0900

    공통: Pretendard 폰트를 추가한다
    https://noonnu.cc/font_page/694

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
